0b4cc3d7353782efdf40a9924bc726208e171997
Рустам Шайхатаров <timergalin@gmail.com>
2022-04-05 11:19:10 +0500

    -

---
+++ Bitrix24RestApiClient/Core/BatchStrategies/ProductRowsBySmartProcessIdsStrategy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Bitrix24RestApiClient.Core.Client;
+using Bitrix24RestApiClient.Core.Models.Enums;
+using Bitrix24RestApiClient.Core.Models.RequestArgs;
+using Bitrix24RestApiClient.Core.Models.Response.BatchResponse;
+using Bitrix24RestApiClient.Core.Models.Response.ListItemsResponse;
+
+namespace Bitrix24RestApiClient.Core.BatchStrategies
+{
+    public class ProductRowsBySmartProcessIdsStrategy
+    {
+        private IBitrix24Client client;
+
+        public ProductRowsBySmartProcessIdsStrategy(IBitrix24Client client)
+        {
+            this.client = client;
+        }
+
+        public async IAsyncEnumerable<ByIdBatchResponseItem<ListProductRowsResponse>> Get(string smartProcessType, List<int> smartProcessIds)
+        {
+            int batchSize = 50;
+
+            for (int i = 0; i < smartProcessIds.Count; i += batchSize)
+            {
+                var partIds = smartProcessIds.GetRange(i, Math.Min(batchSize, smartProcessIds.Count - i));
+
+                await foreach (ByIdBatchResponseItem<ListProductRowsResponse> item in BatchGetItems(smartProcessType, partIds))
+                    yield return item;
+            }
+        } 
+
+        private async IAsyncEnumerable<ByIdBatchResponseItem<ListProductRowsResponse>> BatchGetItems(string smartProcessType, List<int> ids)
+        {
+            CrmBatchRequestArgs getItemsBatch = new CrmBatchRequestArgs() 
+            {
+                Halt = 0,
+                Commands = ids
+                    .Select(id => new { Id = id, Cmd = $"{EntryPointPrefix.CrmProductRow.Value}.{EntityMethod.List.Value}?filter[%3DownerId]={id}&filter[%3DownerType]={smartProcessType}" })
+                    .ToDictionary(x => x.Id.ToString(), x => x.Cmd)
+            }; 
+
+            BatchResponse<ListProductRowsResponse> batchResponse = await client.SendPostRequest<CrmBatchRequestArgs, BatchResponse<ListProductRowsResponse>>(EntryPointPrefix.Batch, EntityMethod.None, getItemsBatch);
+            if (batchResponse.Result.Error.Count > 0)
+                throw new Exception($"Ошибка при выполнении batch-запроса. Ответ: {JsonConvert.SerializeObject(batchResponse)}");
+
+            foreach (ByIdBatchResponseItem<ListProductRowsResponse> item in ids.Select(x => new ByIdBatchResponseItem<ListProductRowsResponse>
+            {
+                Id = x,
+                Result  = batchResponse.Result.Result[x.ToString()]
+            }))
+                yield return item;
+        }
+    }
+}
